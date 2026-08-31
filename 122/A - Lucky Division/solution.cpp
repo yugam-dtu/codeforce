658e09f16ea81891d95b3eff5bf09743968e9c45
Yugam Grover <YUGROV@GMAIL.COM>
2026-08-31 17:59:24 +0530

    Add Lucky Division [A] from Codeforces

---
+++ 122/A - Lucky Division/solution.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+//    4,7,47,74,444,474,477,744,747,777
+    int n;
+    cin>>n;
+    if(n%4==0||n%7==0||n%47==0||n%74==0||n%444==0||n%474==0||n%477==0||n%744==0||n%747==0||n%777==0){
+     cout<<"YES";}
+    else
+    {cout<<"NO";}
+    
+    return 0;
+}
